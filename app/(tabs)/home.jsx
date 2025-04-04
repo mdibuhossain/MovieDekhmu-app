6ba41c90537e9466ce47114f7ad054b53c842e3a
Ibrahim <ibuhossain3@gmail.com>
2025-04-05 01:42:59 +0600

    update

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -6,7 +6,7 @@ import {
   View,
 } from "react-native";
 import CustomInpurField from "../../components/CustomInpurField";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
 import { FlashList } from "@shopify/flash-list";
 import { deleteMovie, getMovies } from "../../lib/firebaseService";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,7 +14,6 @@ import { setDataByIndex, setLoading } from "@/redux/slices/dataSlice";
 import { resetFilterForm, setMovie } from "@/redux/slices/formDataSlice";
 import CustomDropdown from "../../components/CustomDropdown";
 import CustomButton from "../../components/CustomButton";
-import { Swipeable } from "react-native-gesture-handler";
 import Feather from "@expo/vector-icons/Feather";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { useRouter } from "expo-router";
@@ -23,15 +22,20 @@ import LottieView from "lottie-react-native";
 import listLoader from "@/assets/list-loader.json";
 import { Dimensions } from "react-native";
 import { useDebounce } from "use-debounce";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
 
 const Home = () => {
+  const router = useRouter();
   const dispatch = useDispatch();
+  const bottomSheetModalRef = useRef(null);
   const formData = useSelector((state) => state?.formData);
-  const { movies, isLoading } = useSelector((state) => state?.data);
   const [debounceFilter] = useDebounce(formData?.filter, 500);
   const [currentItemIndex, setCurrentItemIndex] = useState(null);
-  const swipeableRefs = useRef([]);
-  const router = useRouter();
+  const { movies, isLoading } = useSelector((state) => state?.data);
 
   const fetchData = (filter) => {
     try {
@@ -55,23 +59,10 @@ const Home = () => {
     fetchData(debounceFilter);
   }, [debounceFilter]);
 
-  const handleSwipeOpen = (index) => {
-    setCurrentItemIndex(index);
-    const openedSwipeableRef = swipeableRefs.current[index];
-    swipeableRefs.current.forEach((_swipeableRef) => {
-      if (_swipeableRef !== openedSwipeableRef) {
-        _swipeableRef.close();
-      }
-    });
-  };
-
   const handleEdit = (item) => {
-    setCurrentItemIndex((_prev) => {
-      swipeableRefs.current[_prev].close();
-      return null;
-    });
     dispatch(setMovie(item));
     router.push("/movieUpdateModal");
+    bottomSheetModalRef.current.dismiss();
   };
 
   const handleDelete = (item) => {
@@ -87,10 +78,6 @@ const Home = () => {
           text: "Delete",
           style: "destructive",
           onPress: () => {
-            setCurrentItemIndex((_prev) => {
-              swipeableRefs.current[_prev].close();
-              return null;
-            });
             deleteMovie(item?.id)
               .then(() => {
                 dispatch(
@@ -107,6 +94,7 @@ const Home = () => {
         },
       ]
     );
+    bottomSheetModalRef.current.dismiss();
   };
 
   const spinner = () => {
@@ -219,43 +207,58 @@ const Home = () => {
         ListHeaderComponent={spinner}
         renderItem={({ item, index }) => {
           return (
-            <Swipeable
-              friction={2}
-              ref={(_ref) => {
-                if (!swipeableRefs.current[index])
-                  swipeableRefs.current[index] = _ref;
+            <TouchableHighlight
+              className="bg-gray-800 px-2 py-1 rounded-md my-0.5"
+              onLongPress={() => {
+                setCurrentItemIndex(index);
+                bottomSheetModalRef.current.present();
               }}
-              renderRightActions={() => renderRightActions(item)}
-              onSwipeableOpen={() => handleSwipeOpen(index)}
             >
-              <TouchableHighlight
-                elevate
-                size="$1"
-                className="bg-gray-800 px-2 py-1 rounded-md my-0.5"
-                onPress={() => {
-                  console.log("date", new Date());
-                }}
-              >
-                <View className="flex-col items-start gap-1">
-                  <View className="flex-row flex-1 items-start text-white text-sm font-[MavenPro-Regular]">
-                    <Text className="flex-1 color-white leading-5">
-                      {item?.title}
-                    </Text>
-                    <Text className="color-gray-400/80 text-xs">
-                      {item?.filmType} ({item?.subType})
-                    </Text>
-                  </View>
-                  <Text className="text-xs text-gray-400 font-[MavenPro-Regular]">
-                    {item?.review} - {item?.origin} - {item?.year}
+              <View className="flex-col items-start gap-1">
+                <View className="flex-row flex-1 items-start text-white text-sm font-[MavenPro-Regular]">
+                  <Text className="flex-1 color-white leading-5">
+                    {item?.title}
+                  </Text>
+                  <Text className="color-gray-400/80 text-xs">
+                    {item?.filmType} ({item?.subType})
                   </Text>
                 </View>
-              </TouchableHighlight>
-            </Swipeable>
+                <Text className="text-xs text-gray-400 font-[MavenPro-Regular]">
+                  {item?.review} - {item?.origin} - {item?.year}
+                </Text>
+              </View>
+            </TouchableHighlight>
           );
         }}
       />
+      <BottomSheetModalProvider>
+        <BottomSheetModal ref={bottomSheetModalRef} snapPoints={["50%"]}>
+          <BottomSheetView>
+            <View className="flex-col justify-around">
+              <CustomButton
+                title="Edit"
+                containerStyle="px-4 py-2 rounded-none bg-transparent"
+                textStyle="text-black font-[MavenPro-Semibold]"
+                handlePress={() => handleEdit(movies[currentItemIndex])}
+              />
+              <CustomButton
+                title="Delete"
+                containerStyle="px-4 py-2 rounded-none bg-transparent"
+                textStyle="text-black font-[MavenPro-Semibold]"
+                handlePress={() => handleDelete(movies[currentItemIndex])}
+              />
+              <CustomButton
+                title="Cancel"
+                containerStyle="px-4 py-2 rounded-none bg-red-600"
+                textStyle="text-white font-[MavenPro-Semibold]"
+                handlePress={() => bottomSheetModalRef.current.dismiss()}
+              />
+            </View>
+          </BottomSheetView>
+        </BottomSheetModal>
+      </BottomSheetModalProvider>
     </View>
   );
 };
 
-export default Home;
+export default memo(Home);
