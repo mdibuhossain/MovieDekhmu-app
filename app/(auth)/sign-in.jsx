5aa87537825d9e4ab03811e019a05bfc2342d31e
mahmud.ponir.9800@gmail.com <ibuhossain3@gmail.com>
2025-04-12 17:02:37 +0600

    updated

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -7,6 +7,7 @@ import { images } from "../../constants";
 import { logIn } from "@/lib/supabaseService";
 import { useDispatch, useSelector } from "react-redux";
 import { setLoading, setUser } from "@/redux/slices/authSlice";
+import { router } from "expo-router";
 
 const initialUserInfo = {
   email: "",
